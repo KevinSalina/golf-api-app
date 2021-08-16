7fd4de2885ed16fc1e8f7f8d34034b0627cf8a7f
Kevin Salina <kevin.salina25@gmail.com>
2021-08-16 09:42:20 -0400

    Created file structure. Set up basic server

---
+++ mocks/golf.js
@@ -0,0 +1,35 @@
+const players = [
+  {
+    name: "Collin Morikawa",
+    points: 2171,
+    events: 20,
+    wins: 2,
+    rank: 1
+  },
+  {
+    name: "Jordan Spieth",
+    points: 2139,
+    events: 22,
+    wins: 1,
+    rank: 2
+  }
+]
+
+const tournaments = [
+  {
+    name: "The Northen Trust",
+    purse: 9500000,
+    location: "Jersey City, New Jersey",
+    course: "Liberty National Golf Course",
+    date: "Aug 19 - Aug 22, 2021"
+  },
+  {
+    name: "BMW Championship",
+    purse: 9500000,
+    location: "Owings Mills, Maryland",
+    course: "Caves Valley Golf Club",
+    date: "Aug 26 - Aug 29, 2021"
+  }
+]
+
+module.exports = { players, tournaments }
--- server.js
+++ server.js
@@ -2,9 +2,17 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.set('view engine', 'pug')
+app.use(express.static('public'))
+app.use(bodyParser.json())
+
 
 app.get('/', (req, res) => {
-  res.send('HELLOOOO')
+
+})
+
+app.get('*', (req, res) => {
+  res.sendStatus(404)
 })
 
 app.listen(port, () => {
